1fcce14d91918ad5f5e3a9b084ca988d17c3dc25
Alexis <jalexismtz.medina@gmail.com>
2020-07-19 15:50:32 -0500

    Actualizacion: se prepara para githubpages

---
+++ docs/precache-manifest.c8496f997591709519c6b5227d554b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bf2fad08f5d6e772bf6e27b6530ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d02d9dcdf58ebf52dc43",
+    "url": "/static/js/2.d65c5a60.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/static/js/2.d65c5a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88601f84e7c3497c37f1",
+    "url": "/static/js/main.84b4634c.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
